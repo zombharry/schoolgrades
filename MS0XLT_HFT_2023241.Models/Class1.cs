8b54be55c7033c9765ebe47359879405472d85a9
bihizsombor <86325422+zombharry@users.noreply.github.com>
2023-10-22 19:54:31 +0200

    Added classes to the model layer

--- MS0XLT_HFT_2023241.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MS0XLT_HFT_2023241.Models
-{
-    public class Class1
-    {
-    }
-}
---
+++ MS0XLT_HFT_2023241.Models/Reservation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MS0XLT_HFT_2023241.Models
+{
+    public class Reservation
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ReservationId { get; set; }
+
+        [Required]
+        public int RoomId { get; set; }
+
+        public int Day { get; set; }
+    }
+}
