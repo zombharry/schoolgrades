bbe59677ef331599da0b83e74cdced8f1bf0a753
bihizsombor <86325422+zombharry@users.noreply.github.com>
2023-11-21 13:14:39 +0100

    GradeRepository added

---
+++ MS0XLT_HFT_2023241.Repository/GradeRepository.cs
@@ -0,0 +1,30 @@
+﻿using MS0XLT_HFT_2023241.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MS0XLT_HFT_2023241.Repository
+{
+    public class GradeRepository : Repository<Grade>, IRepository<Grade>
+    {
+        public GradeRepository(UniversityDbContext context) : base(context)
+        {
+        }
+
+        public override Grade Read(int id)
+        {
+            return this.context.Grades.First(t => t.GradeId == id);
+        }
+
+        public override void Update(Grade item)
+        {
+            var old = Read(item.GradeId);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+        }
+    }
+}
