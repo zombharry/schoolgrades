1ee312a1b720b53908d53a7930a797ab98b13c13
bihizsombor <86325422+zombharry@users.noreply.github.com>
2023-11-21 12:53:58 +0100

    IRepository and abstract Repository class added

---
+++ MS0XLT_HFT_2023241.Repository/Repository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MS0XLT_HFT_2023241.Repository
+{
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        protected UniversityDbContext context;
+        public Repository(UniversityDbContext context)
+        {
+                this.context = context;
+        }
+        public void Create(T item)
+        {
+            context.Set<T>().Add(item);
+            context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            context.Set<T>().Remove(Read(id));
+            context.SaveChanges();
+        }
+
+        public IQueryable<T> ReadAll()
+        {
+            return context.Set<T>();
+        }
+
+        public abstract T Read(int id);
+
+        public abstract void Update(T item);
+    }
+}
