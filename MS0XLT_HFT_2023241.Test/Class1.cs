5bb71fce7a546de6e0ea9342caaee6e637a88155
bihizsombor <86325422+zombharry@users.noreply.github.com>
2023-11-29 01:40:33 +0100

    nuget packages installed for testing

--- MS0XLT_HFT_2023241.Test/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MS0XLT_HFT_2023241.Test
-{
-    public class Class1
-    {
-    }
-}
---
+++ MS0XLT_HFT_2023241.Test/StudentLogicTester.cs
@@ -0,0 +1,23 @@
+﻿using Moq;
+using MS0XLT_HFT_2023241.Logic;
+using MS0XLT_HFT_2023241.Models;
+using MS0XLT_HFT_2023241.Repository;
+using NUnit.Framework;
+using System;
+
+namespace MS0XLT_HFT_2023241.Test
+{
+    [TestFixture]
+    public class StudentLogicTester
+    {
+        StudentLogic logic;
+        Mock<IRepository<Student>> mockStudentRepo;
+        [SetUp]
+        public void Init()
+        {
+            mockStudentRepo = new Mock<IRepository<Student>>();
+
+            logic = new StudentLogic(mockStudentRepo.Object);
+        }
+    }
+}
