cfa9f1bafdce7b3cebb96eb43b7121735f8c7bd7
zombharry <86325422+zombharry@users.noreply.github.com>
2024-04-29 15:43:15 +0200

    wpf project started

---
+++ MS0XLT_HFT_202341.WpfClient/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MS0XLT_HFT_202341.WpfClient
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
